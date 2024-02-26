8ea351ec85d447ed6abc2d8bd988b76ec69c2308
kamilaghayev <kamilizundef@gmail.com>
2024-02-26 23:43:35 +0400

    image fixed

--- src/components/game-cover/GameCover.jsx
+++ src/components/game-cover/GameCover.jsx
@@ -2,8 +2,9 @@ import classNames from "classnames"
 import css from "./gameCover.module.css"
 
 const GameCover = ({ styles, img = ""}) => {
+    console.log(img);
     return (
-        <div className={classNames(styles, css.gameCover)} style={{backgroundImage: `url(/public${img})`}}/>
+        <div className={classNames(styles, css.gameCover)} style={{backgroundImage: `url(/src${img})`}}/>
     )
 }
 
