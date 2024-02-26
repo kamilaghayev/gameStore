60e1c9f42c179b0130830f96e0173120ccd5233f
kamilaghayev <kamilizundef@gmail.com>
2024-02-26 23:52:03 +0400

    bg-changed

--- src/components/game-cover/GameCover.jsx
+++ src/components/game-cover/GameCover.jsx
@@ -4,7 +4,7 @@ import css from "./gameCover.module.css"
 const GameCover = ({ styles, img = ""}) => {
     console.log(img);
     return (
-        <div className={classNames(styles, css.gameCover)} style={{backgroundImage: `url(/src${img})`}}/>
+        <div className={classNames(styles, css.gameCover)} style={{backgroundImage: `url(${img})`}}/>
     )
 }
 
